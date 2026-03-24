af8cea3086d732de4be4d37a0a51232b29df849e
Inhwa Kim <inhwa.kim@kakao.com>
2026-03-24 23:07:55 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -20,20 +20,4 @@ vim.diagnostic.config({
     virtual_lines = { current_line = true },
 })
 
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = vim.api.nvim_create_augroup("my.lsp", {}),
-    callback = function(ev)
-        local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
-
-        if client:supports_method("textDocument/completion") then
-            vim.lsp.completion.enable(true, client.id, ev.buf, {
-                autotrigger = true,
-                convert = function(item)
-                    return { abbr = item.label:gsub("%b()", "") }
-                end,
-            })
-        end
-    end,
-})
-
 require("config.lazy")
