3d78a06665e52fa71687b70f38e10d612a18cf60
Inhwa Kim <inhwa.kim@kakao.com>
2026-03-27 23:42:58 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,16 +1,26 @@
+-- 일반
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.cursorline = true
+vim.opt.scrolloff = 10
+
+-- 들여쓰기
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.softtabstop = 2
+vim.opt.expandtab = true
+
+-- 시각적 정보
+vim.opt.list = true
+vim.opt.signcolumn = "yes"
+vim.opt.showmatch = true
+vim.opt.colorcolumn = { 100 }
+
+-- 키 맵
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.list = true
-vim.opt.cursorline = true
-vim.opt.signcolumn = "yes"
-vim.opt.scrolloff = 10
-vim.opt.showmatch = true
-vim.opt.breakindent = true
-vim.opt.colorcolumn = { 100, 120 }
-
+-- 진단 메시지 표시
 vim.diagnostic.config({
     virtual_lines = { current_line = true },
 })
