33d6860980e1883e0411c7e6ed8e0ca2287aafb8
Inhwa Kim <inhwa.kim@kakao.com>
2026-03-30 19:10:29 +0900

    Add .hammerspoon/Spoons/input.lua Add .hammerspoon/Spoons/vim.lua Add .hammerspoon/.stylua.toml Update .hammerspoon/init.lua

--- dot_hammerspoon/init.lua
+++ dot_hammerspoon/init.lua
@@ -1,57 +1,20 @@
-local ENABLED_APPS = {
-  ["com.google.Chrome"] = true,
-  ["com.jetbrains.intellij"] = true,
-  ["com.microsoft.VSCode"] = true,
-  ["com.mitchellh.ghostty"] = true,
-  ["md.obsidian"] = true,
+local vim = require("Spoons.vim")
+local input = require("Spoons.input")
+
+local TARGET_APPLICATIONS = {
+  "com.google.Chrome",
+  "com.jetbrains.intellij",
+  "com.microsoft.VSCode",
+  "com.mitchellh.ghostty",
+  "md.obsidian",
 }
 
-local INPUT_ENGLISH = "com.apple.keylayout.ABC"
-local INPUT_KOREAN = "com.apple.inputmethod.Korean.2SetKorean"
-
-local function switchToEnglishIfNeeded()
-  if hs.keycodes.currentSourceID() ~= INPUT_ENGLISH then
-    hs.keycodes.currentSourceID(INPUT_ENGLISH)
-  end
-end
-
-local function handleCtrlBracketReleased()
-  switchToEnglishIfNeeded()
-  hs.eventtap.keyStroke({}, "escape")
-end
-
-local ctrlBracketHotkey = hs.hotkey.new({ "ctrl" }, "[", nil, handleCtrlBracketReleased)
-
-local function getFrontmostBundleId()
-  local app = hs.application.frontmostApplication()
-  return app and app:bundleID() or nil
-end
-
-local function shouldEnableForFrontApp()
-  local bundleId = getFrontmostBundleId()
-  return bundleId and ENABLED_APPS[bundleId] == true
-end
-
-local function updateStateOnFocus()
-  if shouldEnableForFrontApp() then
-    ctrlBracketHotkey:enable()
-    switchToEnglishIfNeeded()
-  else
-    ctrlBracketHotkey:disable()
-  end
-end
-
-local appFocusWatcher = hs.window.filter.new()
-appFocusWatcher:subscribe(hs.window.filter.windowFocused, updateStateOnFocus)
-
-updateStateOnFocus()
-
-hs.hotkey.bind({"shift"}, "space", function()
-    local currentSource = hs.keycodes.currentSourceID()
-
-    if currentSource == INPUT_ENGLISH then
-        hs.keycodes.currentSourceID(INPUT_KOREAN)
-    else
-        hs.keycodes.currentSourceID(INPUT_ENGLISH)
-    end
+hs.hotkey.bind({ "ctrl" }, "[", nil, function()
+  vim.handleCtrlOpeningBracketRelease(TARGET_APPLICATIONS)
 end)
+
+hs.window.filter.new():subscribe(hs.window.filter.windowFocused, function()
+  vim.handleWindowFocused(TARGET_APPLICATIONS)
+end)
+
+hs.hotkey.bind({ "shift" }, "space", input.toggleInputSource)
