b5646b45e5c68e1691aab54b683033485386e9fc
Inhwa Kim <inhwa.kim@kakao.com>
2026-04-05 21:43:01 +0900

    Update .config

--- dot_config/nvim/plugin/24-mason-lspconfig.lua
+++
@@ -1,9 +0,0 @@
-vim.pack.add({ "https://github.com/mason-org/mason-lspconfig.nvim" })
-
-require("mason-lspconfig").setup({
-	automatic_enable = {
-		"lua_ls",
-		"tailwindcss",
-		"ts_ls",
-	},
-})
