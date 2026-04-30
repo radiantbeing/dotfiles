82db5ac4528e903a15667a268d5bf93a1072c6e8
Inhwa Kim <inhwa.kim@personalai.so>
2026-04-30 16:27:06 +0900

    Update .config/nvim/init.lua

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,54 +1,36 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
+-- ---------------------------------------------------------
+-- OPTIONS
+-- ---------------------------------------------------------
 
 vim.o.number = true
 vim.o.relativenumber = true
-vim.o.list = true
-vim.o.scrolloff = 10
-vim.opt.colorcolumn = { 100 }
+vim.o.cursorline = true
 vim.o.signcolumn = "yes"
+vim.o.scrolloff = 10
+vim.o.breakindent = true
+vim.o.list = true
+vim.o.showmatch = true
+vim.opt.colorcolumn = { 100 }
+
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+vim.o.confirm = true
+
+-- ---------------------------------------------------------
+-- KEYMAPS
+-- ---------------------------------------------------------
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+-- ---------------------------------------------------------
+-- PLUGINS
+-- ---------------------------------------------------------
 
 vim.pack.add({
-  { src = "https://github.com/catppuccin/nvim", name = "catppuccin" },
-  "https://github.com/NMAC427/guess-indent.nvim",
-  "https://github.com/mason-org/mason.nvim",
-  "https://github.com/neovim/nvim-lspconfig",
-  "https://github.com/mason-org/mason-lspconfig.nvim",
-  "https://github.com/folke/lazydev.nvim",
-  { src = "https://github.com/obsidian-nvim/obsidian.nvim", version = vim.version.range("*") },
-  "https://github.com/nvim-lua/plenary.nvim",
-  "https://github.com/nvim-telescope/telescope.nvim"
+	{ src = "https://github.com/catppuccin/nvim", name = "catppuccin" }
 })
 
-vim.cmd.colorscheme("catppuccin-macchiato")
-
-require("guess-indent").setup({})
-
-require("mason").setup({})
-require("mason-lspconfig").setup({
-  ensure_installed = {
-    "lua_ls",
-    "vimls",
-    "ts_ls"
-  }
-})
-
-require("lazydev").setup({
-  library = {
-    { path = "${3rd}/luv/library", words = { "vim%.uv" } }
-  }
-})
-
-require("obsidian").setup({
-  legacy_commands = false,
-  workspaces = {
-    { name = "personalai", path = "~/Documents/vaults/personalai" }
-  }
-})
-
-local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+vim.cmd.colorscheme("catppuccin-frappe")
 
