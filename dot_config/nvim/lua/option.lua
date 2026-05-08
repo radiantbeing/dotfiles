81724b7b7b0832d5651765e4e0d670eb48bb2e04
Inhwa Kim <inhwa.kim@personalai.so>
2026-05-08 12:48:04 +0900

    Remove .config/nvim/.keep Remove .config/nvim/after/ftplugin/markdown.lua Remove .config/nvim/ftplugin/javascript.lua Remove .config/nvim/ftplugin/javascriptreact.lua Remove .config/nvim/ftplugin/lua.lua Remove .config/nvim/ftplugin/typescript.lua Remove .config/nvim/ftplugin/typescriptreact.lua Remove .config/nvim/init.lua Remove .config/nvim/lua/option.lua Remove .config/nvim/lua/plugin.lua Remove .config/nvim/lua/plugins/colorscheme.lua Remove .config/nvim/lua/plugins/completion.lua Remove .config/nvim/lua/plugins/fuzzyfinder.lua Remove .config/nvim/lua/plugins/guessindent.lua Remove .config/nvim/lua/plugins/init.lua Remove .config/nvim/lua/plugins/lazydev.lua Remove .config/nvim/lua/plugins/lsp.lua Remove .config/nvim/lua/plugins/obsidian.lua Remove .config/nvim/lua/plugins/statusline.lua Remove .config/nvim/lua/plugins/treesitter.lua Remove .config/nvim/nvim-pack-lock.json Update .zshrc

--- dot_config/nvim/lua/option.lua
+++
@@ -1,6 +0,0 @@
-vim.o.number = true
-vim.o.relativenumber = true
-vim.o.list = true
-vim.o.showmode = false
-vim.opt.colorcolumn = { 100 }
-
--- dot_config/nvim/lua/plugins/init.lua
+++
@@ -1,9 +0,0 @@
-require("plugins.colorscheme")
-require("plugins.statusline")
-require("plugins.fuzzyfinder")
-require("plugins.lsp")
-require("plugins.lazydev")
-require("plugins.completion")
-require("plugins.guessindent")
-require("plugins.obsidian")
-
