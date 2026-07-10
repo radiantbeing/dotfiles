d84e8363a26175cff5d1665d38cf0ccfa21e5f30
Inhwa Kim <inhwa.kim@personalai.so>
2026-07-10 11:51:47 +0900

    Hammerspoon 스크립트 리팩터링

--- .hammerspoon/Spoons/vim.lua
+++ .hammerspoon/Spoons/vim.lua
@@ -1,33 +1,37 @@
-local InputSource = {
-  ENGLISH = "com.apple.keylayout.ABC",
-  KOREAN = "com.apple.inputmethod.Korean.2SetKorean",
-}
+local ENGLISH_INPUT_SOURCE = "com.apple.keylayout.ABC"
 
-local function isFrontmostApplicationIn(targetApplications)
-  local frontmostApplicationBundleId = hs.application.frontmostApplication():bundleID()
-  for _, bundleId in ipairs(targetApplications) do
-    if bundleId == frontmostApplicationBundleId then
+local function isFrontmostAppIn(bundleIds)
+  local frontmost = hs.application.frontmostApplication():bundleID()
+  for _, bundleId in ipairs(bundleIds) do
+    if bundleId == frontmost then
       return true
     end
   end
   return false
 end
 
-local M = {}
-
-function M.handleCtrlOpeningBracketRelease(targetApplications)
-  if not isFrontmostApplicationIn(targetApplications) then
-    return
-  end
-  hs.keycodes.currentSourceID(InputSource.ENGLISH)
-  hs.eventtap.keyStroke({}, "escape")
+local function switchToEnglish()
+  hs.keycodes.currentSourceID(ENGLISH_INPUT_SOURCE)
 end
 
-function M.handleWindowFocused(targetApplications)
-  if not isFrontmostApplicationIn(targetApplications) then
-    return
-  end
-  hs.keycodes.currentSourceID(InputSource.ENGLISH)
+local windowFilter
+
+local M = {}
+
+function M.enableFor(bundleIds)
+  hs.hotkey.bind({ "ctrl" }, "[", nil, function()
+    if isFrontmostAppIn(bundleIds) then
+      switchToEnglish()
+      hs.eventtap.keyStroke({}, "escape")
+    end
+  end)
+
+  windowFilter = hs.window.filter.new()
+  windowFilter:subscribe(hs.window.filter.windowFocused, function()
+    if isFrontmostAppIn(bundleIds) then
+      switchToEnglish()
+    end
+  end)
 end
 
 return M
--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,15 +1,7 @@
 local vim = require("Spoons.vim")
 
-local TARGET_APPLICATIONS = {
+vim.enableFor({
   "com.google.Chrome",
   "net.kovidgoyal.kitty",
   "org.mozilla.firefox",
-}
-
-hs.hotkey.bind({ "ctrl" }, "[", nil, function()
-  vim.handleCtrlOpeningBracketRelease(TARGET_APPLICATIONS)
-end)
-
-hs.window.filter.new():subscribe(hs.window.filter.windowFocused, function()
-  vim.handleWindowFocused(TARGET_APPLICATIONS)
-end)
+})
