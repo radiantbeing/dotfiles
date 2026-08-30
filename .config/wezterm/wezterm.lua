2fa0ad65b4c09663f44d96f801941e4f62219f5b
김인화 <inhwakim@serius.local>
2026-08-30 22:27:35 +0900

    WezTerm 구성 추가

---
+++ .config/wezterm/wezterm.lua
@@ -0,0 +1,9 @@
+local wezterm = require("wezterm")
+local config = {}
+
+config.color_scheme = "Catppuccin Mocha"
+config.font = wezterm.font("Sarasa Mono K")
+config.font_size = 16.0
+
+return config
+
