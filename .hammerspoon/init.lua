974769839f6251c5372ae6842ed37231cd22c702
김인화 <inhwakim@serius.local>
2026-08-30 22:27:06 +0900

    WezTerm 추가

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,8 +1,9 @@
 local ime = require("Spoons.ime")
 
 ime.enableFor({
+  "com.github.wez.wezterm",
   "com.google.Chrome",
+  "md.obsidian",
   "net.kovidgoyal.kitty",
   "org.mozilla.firefox",
-  "md.obsidian",
 })
