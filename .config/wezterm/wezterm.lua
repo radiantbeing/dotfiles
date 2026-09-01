6f524ab0586812f12c6d5cf606080241d7e927ae
Inhwa Kim <inhwa.kim@personalai.so>
2026-09-01 09:29:21 +0900

    `Sarasa Term K` 글꼴 적용

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.color_scheme = "Catppuccin Mocha"
-config.font = wezterm.font("Sarasa Mono K")
+config.font = wezterm.font("Sarasa Term K")
 config.font_size = 16.0
 
 return config
