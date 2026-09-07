762cbdeb46293182b39687e555105b281ea4c1ee
Inhwa Kim <inhwa.kim@personalai.so>
2026-09-07 16:32:57 +0900

    Enable Unicode NFC normalization in WezTerm

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,6 +4,7 @@ local config = {}
 config.color_scheme = "Catppuccin Mocha"
 config.font = wezterm.font("Sarasa Term K")
 config.font_size = 16.0
+config.normalize_output_to_unicode_nfc = true
 
 return config
 
