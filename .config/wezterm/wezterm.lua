774030bf8911a060e485e3034e502689d4abada3
Inhwa Kim <inhwa.kim@personalai.so>
2026-09-07 16:53:51 +0900

    Add WezTerm pane cycle keys and disable Alt+Enter fullscreen.

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -6,5 +6,23 @@ config.font = wezterm.font("Sarasa Term K")
 config.font_size = 16.0
 config.normalize_output_to_unicode_nfc = true
 
+config.keys = {
+  {
+    key = "}",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Next"),
+  },
+  {
+    key = "{",
+    mods = "CTRL|SHIFT",
+    action = wezterm.action.ActivatePaneDirection("Prev"),
+  },
+  {
+    key = "Enter",
+    mods = "ALT",
+    action = wezterm.action.DisableDefaultAssignment,
+  },
+}
+
 return config
 
